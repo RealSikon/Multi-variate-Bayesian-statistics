ea60c5d8f5633db56b721f4664202dc8875d7e74
schoolshooter69 <silasbachmann@gmail.com>
2024-05-03 11:32:52 +0200

    formatting + attempt at creating a python script
    datasyn...mode.py does not work, only notebook does

--- hornby&hu/AttributeDisclosureRisk.R
+++ hornby&hu/AttributeDisclosureRisk.R
@@ -33,10 +33,10 @@ real_data <- data_loader(real_data_name, 200)
 syn_data  <- data_loader(syn_data_name, 200)
 
 # Load JSON data, Contains: [meta, attribute description, Bayesian network, conditional probabilities]
-privbayes_model <- fromJSON(file.path(data_directory, "description.json"))
-meta <- privbayes_model[[1]] # [#tuples],[#attributes],[#attributesInBN],[attributeList],[candidateKeyList],[nonCategoricalStringAttributeList],[attributesInBN]]
-attribute_description <- privbayes_model[[2]] # unused
-bayesian_network <- privbayes_model[[3]] # [[child], [[parent_1], [parent_2], ... [parent_k]]]
+privbayes_model           <- fromJSON(file.path(data_directory, "description.json"))
+meta                      <- privbayes_model[[1]] # [#tuples],[#attributes],[#attributesInBN],[attributeList],[candidateKeyList],[nonCategoricalStringAttributeList],[attributesInBN]]
+attribute_description     <- privbayes_model[[2]] # unused
+bayesian_network          <- privbayes_model[[3]] # [[child], [[parent_1], [parent_2], ... [parent_k]]]
 conditional_probabilities <- privbayes_model[[4]] # unused
 
 
@@ -93,37 +93,37 @@ risk_list <- list()
 
 for (formular in seq_along(formulars)) {
   risk_list[[formular]] <- AttributeRisk(
-    modelFormulas = string_parser(paste("list(", formulars[[formular]], ")", sep = "")),
-    origdata = real_data,
-    syndata = syn_data,
+     modelFormulas = string_parser(paste("list(", formulars[[formular]], ")", sep = "")),
+          origdata = real_data,
+           syndata = syn_data,
     posteriorMCMCs = draws_conts[formular],
-    syntype = c("norm", "norm"),
-    G = c(11, 11),
-    H = 1
-  )
+           syntype = c("norm", "norm"),
+                 G = c(11, 11),
+                 H = 1
+    )
 }
 
 print("Generating plots")
-
 #' Produces a graph of the probabilities of each guess with a line indicating
 #' the chance of randomly guessing the confidential value(s) from among the
 #' guesses.
-#' Density of the joint posterior probability of correctly guessing the true
-#' value of both LogIncome and LogExpenditure. The vertical line shows the prior probability of 1/121.
 for (risk in seq_along(risk_list)) {
-  plot_name <- paste("randomGuess_", nodes[[risk]], ".png", sep = "")
-  ggsave(plot_name, device = "png", randomGuessPlot(risk_list[[risk]]), path = plot_directory)
+  #' Density of the joint posterior probability of correctly guessing the true
+  #' value of both LogIncome and LogExpenditure. The vertical line shows the prior probability of 1/121.
+  ggsave(paste(name, nodes[[risk]], ".png", sep = ""),
+         device = "png",
+         randomGuessPlot(risk_list[[risk]]),
+         path = plot_directory
+         )
+         
+  #' The rank of posterior probability of the true pair of values
+  #' (of LogIncome and LogExpenditure being guessed correctly, among 121 (11*11) guesses)
+  ggsave(paste(name, nodes[[risk]], ".png", sep = ""),
+         device = "png",
+         posteriorRankPlot(risk_list[[risk]]),
+         path = plot_directory
+         )
 }
-
-#' The rank of posterior probability of the true pair of values
-#' (of LogIncome and LogExpenditure being guessed correctly, among 121 (11*11) guesses)
-for (risk in seq_along(risk_list)) {
-  plot_name <- paste("posteriorRank_", nodes[[risk]], ".png", sep = "")
-  ggsave(plot_name, device = "png", posteriorRankPlot(risk_list[[risk]]), path = plot_directory)
-}
-
-# Print first two plots for all formulas
-
 
 # marginalPosteriorProbabilitiesPlot(risks[[i]]):
 #' Density of the marginal posterior probabilities of correctly guessing the
